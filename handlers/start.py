17a3f96f4bc55f50dc7ff238e86a7bb8a1408dba
ZaenProjec <91957490+Zaen-ubot@users.noreply.github.com>
2021-11-01 12:56:31 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,155 +1,87 @@
-from datetime import datetime
-from sys import version_info
-from time import time
+from pyrogram import Client, filters, types
+from solidAPI import emoji
 
-from config import (
-    ALIVE_IMG,
-    ALIVE_NAME,
-    BOT_NAME,
-    BOT_USERNAME,
-    GROUP_SUPPORT,
-    OWNER_NAME,
-    UPDATES_CHANNEL,
-)
-from handlers import __version__
-from helpers.decorators import sudo_users_only
-from helpers.filters import command
-from pyrogram import Client, filters
-from pyrogram import __version__ as pyrover
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from base.client_base import bot
+from utils.functions import get_yt_details, download_yt_thumbnails
 
-__major__ = 0
-__minor__ = 2
-__micro__ = 1
-
-__python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
+markup_keyboard = types.InlineKeyboardMarkup
+button_keyboard = types.InlineKeyboardButton
 
 
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60 * 60 * 24),
-    ("hour", 60 * 60),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
-    return ", ".join(parts)
-
-
-@Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.private & ~filters.edited
-)
-async def start_(client: Client, message: Message):
-    await message.reply_text(
-        f"""✨ **Welcome {message.from_user.mention()} !**\n
-💭 **[{BOT_NAME}](https://t.me/{BOT_USERNAME}) allows you to play music on groups through the new Telegram's voice chats!**
-
-💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
-
-🔖 **To know how to use this bot, please click on the » ❓ Basic Guide button!**""",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "➕ Add me to your Group ➕",
-                        url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+@Client.on_message(filters.command("start"))
+async def start_(_, message: types.Message):
+    bot_username = (await bot.get_me()).username
+    user_id = message.from_user.id
+    if message.chat.type == "supergroup":
+        return await message.reply(
+            f"{emoji.SPARKLES} hi {message.from_user.mention}!\n"
+            f"i'm solidmusic bot, make me as an admin, so i can play musics in this chat's"
+        )
+    if message.chat.type == "private":
+        if len(message.command) == 1:
+            await message.reply(
+                f"hi {message.from_user.mention}! i can play musics on your groups through"
+                "telegram voice chats\n"
+                f"{emoji.LIGHT_BULB} find out all my command by clicking \"commands\" button",
+                reply_markup=markup_keyboard(
+                    [
+                        [
+                            button_keyboard(
+                                f"{emoji.PLUS} add me to your chats",
+                                url=f"https://t.me/{bot_username}?startgroup=true")
+                        ],
+                        [
+                            button_keyboard(
+                                f"{emoji.NOTEBOOK} commands",
+                                callback_data="help_commands"
+                            )
+                        ],
+                        [
+                            button_keyboard(
+                                f"{emoji.LOUDSPEAKER} channel",
+                                url="https://t.me/solidprojects"
+                            )
+                        ],
+                        [
+                            button_keyboard(
+                                f"{emoji.FIRE} maintainer",
+                                url="https://t.me/talktome_bbot"
+                            )
+                        ]
+                    ]
+                )
+            )
+        elif len(message.command) == 2:
+            query = message.command[1]
+            if query.startswith("ytinfo_"):
+                yt_link = query.split("ytinfo_")[1]
+                details = get_yt_details(yt_link)
+                thumb_url = details["thumbnails"]
+                thumb_file = download_yt_thumbnails(thumb_url, user_id)
+                result_text = (
+                    f"**track information**\n\n"
+                    f"{emoji.LABEL} title: {details['title']}\n"
+                    f"{emoji.MEGAPHONE} channel: {details['channel']}\n"
+                    f"{emoji.STOPWATCH} duration: {details['duration']}\n"
+                    f"{emoji.THUMBS_UP} likes: {details['likes']}\n"
+                    f"{emoji.THUMBS_DOWN} dislikes: {details['dislikes']}\n"
+                    f"{emoji.STAR} rating: {details['rating']}\n"
+                )
+                await message.reply_photo(
+                    thumb_file, caption=result_text, reply_markup=markup_keyboard(
+                        [
+                            [
+                                button_keyboard(
+                                    f"{emoji.MOVIE_CAMERA} watch on youtube",
+                                    url=f"{details['link']}"
+                                )
+                            ],
+                            [
+                                button_keyboard(
+                                    f"{emoji.WASTEBASKET} close",
+                                    callback_data="cls"
+                                )
+                            ]
+                        ]
                     )
-                ],
-                [InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")],
-                [
-                    InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
-                    InlineKeyboardButton("❤️ Donate", url=f"https://t.me/{OWNER_NAME}"),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}"
-                    ),
-                    InlineKeyboardButton(
-                        "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(
-                        "🌐 Source Code", url="https://github.com/Zaen-ubot/ZenMusic"
-                    )
-                ],
-            ]
-        ),
-        disable_web_page_preview=True,
-    )
-
-
-@Client.on_message(
-    command(["start", f"start@{BOT_USERNAME}"]) & filters.group & ~filters.edited
-)
-async def start(client: Client, message: Message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-
-    keyboard = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton("✨ Group", url=f"https://t.me/{GROUP_SUPPORT}"),
-                InlineKeyboardButton(
-                    "📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}"
-                ),
-            ]
-        ]
-    )
-
-    alive = f"**Hello {message.from_user.mention()}, i'm {BOT_NAME}**\n\n✨ Bot is working normally\n🍀 My Master: [{ALIVE_NAME}](https://t.me/{OWNER_NAME})\n✨ Bot Version: `v{__version__}`\n🍀 Pyrogram Version: `{pyrover}`\n✨ Python Version: `{__python_version__}`\n🍀 Uptime Status: `{uptime}`\n\n**Thanks for Adding me here, for playing music on your Group voice chat** ❤"
-
-    await message.reply_photo(
-        photo=f"{ALIVE_IMG}",
-        caption=alive,
-        reply_markup=keyboard,
-    )
-
-
-@Client.on_message(
-    command(["help", f"help@{BOT_USERNAME}"]) & filters.group & ~filters.edited
-)
-async def help(client: Client, message: Message):
-    await message.reply_text(
-        f"""✨ **Hello** {message.from_user.mention()} !
-
-» **press the button below to read the explanation and see the list of available commands !**
-
-⚡ __Powered by {BOT_NAME} A.I__""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="❓ Basic Guide", callback_data="cbguide")]]
-        ),
-    )
-
-
-@Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
-async def ping_pong(client: Client, message: Message):
-    start = time()
-    m_reply = await message.reply_text("pinging...")
-    delta_ping = time() - start
-    await m_reply.edit_text("🏓 `PONG!!`\n" f"⚡️ `{delta_ping * 1000:.3f} ms`")
-
-
-@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
-@sudo_users_only
-async def get_uptime(client: Client, message: Message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-    await message.reply_text(
-        "🤖 bot status:\n"
-        f"• **uptime:** `{uptime}`\n"
-        f"• **start time:** `{START_TIME_ISO}`"
-    )
+                )
